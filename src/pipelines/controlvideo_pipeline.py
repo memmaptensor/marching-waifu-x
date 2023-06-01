81e568e3d3a96ce772bd7e15bb38a78b039b3eaa
rossiyareich <tawinjira@hotmail.com>
2023-06-01 19:53:49 +0700

    changed to model cpu offload

--- src/pipelines/controlvideo_pipeline.py
+++ src/pipelines/controlvideo_pipeline.py
@@ -100,7 +100,8 @@ class controlvideo_pipeline:
 
         pipe.enable_vae_slicing()
         pipe.enable_xformers_memory_efficient_attention()
-        pipe.enable_sequential_cpu_offload()
+        pipe.enable_model_cpu_offload()
+        # pipe.enable_sequential_cpu_offload()
 
         # Inference
         if window_size is None:
